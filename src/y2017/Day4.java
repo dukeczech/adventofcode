fe669ef7a9b9e7bffafd1b2912827d85f042dcaf
hebik <hebik@hebik-LIFEBOOK-S762>
2017-12-07 16:28:46 +0100

    Day 4 implemented

---
+++ src/y2017/Day4.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package y2017;
+
+import adventofcode.TaskSolver;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hebik
+ */
+public class Day4 extends TaskSolver {
+
+    private String input = null;
+
+    public Day4() {
+        try {
+            input = new String(Files.readAllBytes(Paths.get("input/Day4-2017.txt")));
+        } catch (IOException ex) {
+            Logger.getLogger(Day4.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void part1() {
+        int valid = 0;
+        Scanner sc = new Scanner(input);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] words = line.split("\\s+");
+            if (words.length == Arrays.stream(words).distinct().count()) {
+                valid++;
+            }
+        }
+        sc.close();
+
+        System.out.println("Valid passphrases: " + valid);
+    }
+
+    private void part2() {
+        int valid = 0;
+        Scanner sc = new Scanner(input);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] words = line.split("\\s+");
+            if (words.length == Arrays.stream(words).map(w -> new WorldWrapper(w)).distinct().count()) {
+                valid++;
+            }
+        }
+        sc.close();
+
+        System.out.println("Valid passphrases: " + valid);
+    }
+
+    @Override
+    protected void process() {
+        part1();
+        part2();
+    }
+
+    class WorldWrapper {
+
+        String world = null;
+
+        public WorldWrapper(String w) {
+            world = w;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof WorldWrapper)) {
+                return false;
+            }
+            WorldWrapper other = (WorldWrapper) obj;
+            if (Objects.equals(world, other.world)) {
+                return true;
+            }
+            if (world.length() != other.world.length()) {
+                return false;
+            }
+            int[] o1 = world.chars().sorted().toArray();
+            int[] o2 = other.world.chars().sorted().toArray();
+            if (Arrays.equals(o1, o2)) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return 1;
+        }
+    }
+}
